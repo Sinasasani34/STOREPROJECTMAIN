07016679ae50d8c4eb961f99708b79d28f011eb9
Sina Sasani <98429680+Sinasasani34@users.noreply.github.com>
2025-03-11 00:15:23 +0330

    resume

--- app/socket.io/index.js
+++ app/socket.io/index.js
@@ -3,5 +3,6 @@ const NameSpaceSocketHandller = require("./namespace.socket")
 module.exports = {
     socketHandller: (io) => {
         new NameSpaceSocketHandller(io).initConnection()
+        new NameSpaceSocketHandller(io).createNamespacesConnection()
     }
 }
--- app/socket.io/namespace.socket.js
+++ app/socket.io/namespace.socket.js
@@ -1,3 +1,5 @@
+const { conversationModel } = require("../models/conversation");
+
 module.exports = class NameSpaceSocketHandller {
     #io;
     constructor(io) {
@@ -5,8 +7,38 @@ module.exports = class NameSpaceSocketHandller {
     }
 
     initConnection() {
-        this.#io.on("connection", (socket) => {
-            console.log(socket.rooms);
+        this.#io.on("connection", async (socket) => {
+            const namespaces = await conversationModel.find({}, { title: 1, endpoint: 1 }).sort({ _id: -1 })
+            socket.emit("namespacesList", namespaces);
         })
+    }
+
+    async createNamespacesConnection() {
+        const namespaces = await conversationModel.find({}, { title: 1, endpoint: 1, rooms: 1 }).sort({ _id: -1 })
+        for (const namespace of namespaces) {
+            this.#io.of(`/${namespace.endpoint}`).on("connection", async (socket) => {
+                const conversation = await conversationModel.findOne({ endpoint: namespace.endpoint }, { rooms: 1 }).sort({ _id: -1 })
+                socket.emit("roomList", conversation.rooms)
+                socket.on("joinRoom", async roomName => {
+                    const lastRoom = Array.from(socket.rooms)[1];
+                    if (lastRoom) {
+                        socket.leave(lastRoom)
+                        await this.getCountofOnlineUsers(namespace.endpoint, roomName)
+                    }
+                    socket.join(roomName)
+                    await this.getCountofOnlineUsers(namespace.endpoint, roomName)
+                    const roomInfo = conversation.rooms.find(item => item.name === roomName)
+                    socket.emit("roomInfo", roomInfo)
+                    socket.on("disconnect", async () => {
+                        await this.getCountofOnlineUsers(namespace.endpoint, roomName)
+                    })
+                })
+            });
+        }
+    }
+
+    async getCountofOnlineUsers(endpoint, roomName) {
+        const onlineUsers = await this.#io.of(`/${endpoint}`).in(roomName).allSockets();
+        this.#io.of(`/${endpoint}`).in(roomName).emit("countOfOnlineUsers", Array.from(onlineUsers).length)
     }
 }
